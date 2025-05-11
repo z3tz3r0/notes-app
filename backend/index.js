005e4f5ce3101efa2dadf38dea26bd6e5e15f449
z3tz3r0 <kittipan.wang@gmail.com>
2025-05-11 21:17:56 +0700

    chore(backend): update index file

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,73 @@
 const express = require("express");
-const app = express();
 const dotenv = require("dotenv");
-const noteRoute = require("./api/v1/noteRoute");
+const errorHandler = require("./utils/errMessage");
+const limiter = require("./middlewares/ratelimiter");
+const path = require("path");
+const { fileURLToPath } = require("url");
+const connectMongoDB = require("./config/mongo");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const router = require("./routes/index.routes");
 
+const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
+app.set("trust proxy", 1);
+
+app.use(helmet());
+
+const whitelist = [
+  "http://localhost:5173",
+  "https://notes-app-silk-seven.vercel.app",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(limiter);
+
 // Built-in json request parser
 app.use(express.json());
+app.use(cookieParser());
 
-// Root route welcoming message
-app.get("/", (req, res) => {
-  res.send("Welcome to notes API endpoint, please navigate to /notes");
-});
+// Pulling welcome page from public folder
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "public")));
 
 // Route Middleware
-app.use("/api/v1/notes", noteRoute);
+app.use("/api/v1/", router);
 
 // Global Error Handling
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
-  const newError = {
-    success: false,
-    statusCode,
-    reason: message,
-  };
-
-  if (process.env.NODE_ENV === "development") {
-    newError.stack = err.stack;
-  }
-  console.error(newError);
-  res.status(statusCode).json(newError);
-});
+app.use(errorHandler);
 
 // Let the server running, wait and listen for the request.
-app.listen(PORT, () => {
-  console.log(`Server is now running at http://localhost:${PORT}`);
+(async () => {
+  try {
+    await connectMongoDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Error starting up the server");
+    console.error(error.message);
+    process.exit(1);
+  }
+})();
+
+// Handle unhandled promise rejections globally
+process.on("unhandledRejection", (err) => {
+  console.error("💥 Unhandled Rejection:", err.message);
+  process.exit(1);
 });
