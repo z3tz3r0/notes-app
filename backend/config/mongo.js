9ac66103cc0b7c559ff666261c3e1acc7cb5a7da
z3tz3r0 <kittipan.wang@gmail.com>
2025-05-12 14:58:42 +0700

    feat: Update MongoDB connection string variable name

--- backend/config/mongo.js
+++ backend/config/mongo.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log("Error connecting to MongoDB", error);
